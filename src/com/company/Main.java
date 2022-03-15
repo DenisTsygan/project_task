f80eeba356cb76083bc7c3598ca48d272e64bda0
DenisTsygan <denis.www.denis7@gmail.com>
2022-03-15 22:43:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_BLACK+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+ANSI_WHITE+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+        System.out.println(ANSI_WHITE +"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE"+"\u25AE" +ANSI_RESET);
+    }
+}
